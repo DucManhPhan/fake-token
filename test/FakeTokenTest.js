650a75364a4f88778b8e17347718b1cca9617bf3
manhpd <ducmanhphan93@gmail.com>
2022-06-18 00:25:36 +0700

    add base test case

---
+++ test/FakeTokenTest.js
@@ -0,0 +1,19 @@
+
+
+constract("FakeToken", (accounts) => {
+
+    let contractInstance;
+
+    it("", async () => {
+
+    })
+
+    it("", async () => {
+
+    })
+
+    it("", async () => {
+
+    })
+
+})
